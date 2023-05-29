d025a9a5f1c45bf08dfeaf2f2042a198e8f3794e
Alejandro Garduño Macedo <alejandro_garma@outlook.com>
2023-05-28 18:59:59 -0600

    Modify init.sh to retun package manager variable

--- init.sh
+++ init.sh
@@ -1,33 +1,47 @@
 #!/bin/bash
 
-# Detect package manager for Linux or macOS
+# Function to detect package manager for Linux or macOS
+detect_package_manager() {
+    # Check if running on macOS
+    if [[ "$(uname)" == "Darwin" ]]; then
+        echo "Detected macOS"
+        if [[ -x "$(command -v brew)" ]]; then
+            echo "Package manager: Homebrew"
+            return 0
+        else
+            echo "Package manager not found"
+            return 1
+        fi
+    # Check if running on Linux
+    elif [[ "$(uname)" == "Linux" ]]; then
+        # Detect package manager based on known package management tools
+        if [[ -x "$(command -v apt-get)" ]]; then
+            echo "Detected Debian-based Linux"
+            echo "Package manager: apt-get"
+            return 0
+        elif [[ -x "$(command -v dnf)" ]]; then
+            echo "Detected Fedora-based Linux"
+            echo "Package manager: DNF"
+            return 0
+        elif [[ -x "$(command -v yum)" ]]; then
+            echo "Detected CentOS/RHEL-based Linux"
+            echo "Package manager: YUM"
+            return 0
+        elif [[ -x "$(command -v pacman)" ]]; then
+            echo "Detected Arch Linux"
+            echo "Package manager: Pacman"
+            return 0
+        else
+            echo "Package manager not found"
+            return 1
+        fi
+    else
+        echo "Unsupported operating system"
+        return 1
+    fi
+}
 
-# Check if running on macOS
-if [[ "$(uname)" == "Darwin" ]]; then
-    echo "Detected macOS"
-    if [[ -x "$(command -v brew)" ]]; then
-        echo "Package manager: Homebrew"
-    else
-        echo "Package manager not found"
-    fi
-# Check if running on Linux
-elif [[ "$(uname)" == "Linux" ]]; then
-    # Detect package manager based on known package management tools
-    if [[ -x "$(command -v apt-get)" ]]; then
-        echo "Detected Debian-based Linux"
-        echo "Package manager: apt-get"
-    elif [[ -x "$(command -v dnf)" ]]; then
-        echo "Detected Fedora-based Linux"
-        echo "Package manager: DNF"
-    elif [[ -x "$(command -v yum)" ]]; then
-        echo "Detected CentOS/RHEL-based Linux"
-        echo "Package manager: YUM"
-    elif [[ -x "$(command -v pacman)" ]]; then
-        echo "Detected Arch Linux"
-        echo "Package manager: Pacman"
-    else
-        echo "Package manager not found"
-    fi
-else
-    echo "Unsupported operating system"
-fi
+# Assign package manager detector to a variable
+package_manager=$(detect_package_manager)
+
+echo "Package manager: $package_manager"
