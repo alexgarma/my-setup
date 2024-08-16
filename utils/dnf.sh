1ed2863ed42dd64d5d2a697608a8d3d4235a292b
Alejandro Garduño Macedo <alejandro_garma@outlook.com>
2024-08-16 08:32:58 -0600

    Modify yum

--- utils/dnf.sh
+++ utils/dnf.sh
@@ -159,3 +159,20 @@ if [ $tmux = true ]; then
     fi
 
 fi
+
+#TODO: Install neovim
+## Install NeoVim if not already present
+#if ! command -v nvim &>/dev/null; then
+#    echo "NeoVim not found. Downloading and installing..."
+#    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+#    chmod u+x nvim.appimage
+#    ./nvim.appimage
+#fi
+#
+## Check if NeoVim is installed successfully
+#if ! command -v nvim &>/dev/null; then
+#    echo "NeoVim installation failed. Extracting and running the app image..."
+#    ./nvim.appimage --appimage-extract
+#    ./squashfs-root/AppRun --version
+#fi
+# TODO: Make neovim the default editor
